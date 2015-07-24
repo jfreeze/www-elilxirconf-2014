2500c45ccc2019a6cba93f514e574754acddaf66
Jim Freeze <jimfreeze@gmail.com>
2015-07-24 07:57:48 -0500

    updates for 2015

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+#set :bind, '0.0.0.0'
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
   #redirect '/index.html'
+end
+
+get '/2014' do
+  send_file File.join(settings.public_folder, '2014.html')
 end
