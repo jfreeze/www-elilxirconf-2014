c904cfb374d14c18b5238c4f3835d9540faf8b45
Jim Freeze <jimfreeze@JimsDevMBP.local>
2016-05-02 12:16:02 -0500

    added trainers

--- app.rb
+++ app.rb
@@ -1,10 +1,30 @@
 require 'sinatra'
-#set :bind, '0.0.0.0'
+set :bind, '0.0.0.0'
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
   #redirect '/index.html'
 end
 
+get '/training' do
+  send_file File.join(settings.public_folder, '2016/speakers.html')
+end
+
+get '/speakers' do
+  send_file File.join(settings.public_folder, '2016/bio.html')
+end
+
+get '/bio' do
+  send_file File.join(settings.public_folder, '2016/bio.html')
+end
+
+get '/coc' do
+  send_file File.join(settings.public_folder, '2016/coc.html')
+end
+
 get '/2014' do
   send_file File.join(settings.public_folder, '2014.html')
+end
+
+get '/2015' do
+  send_file File.join(settings.public_folder, '2015.html')
 end
